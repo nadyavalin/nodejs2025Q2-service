8c46b2153b34eec3887a98c09530bd8ea54b1ac6
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-06-09 10:53:35 +0300

    feat: add files after prettier

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -5,15 +5,15 @@ import { AppModule } from './app.module';
 let app: NestApplication;
 
 export async function setup() {
-    const module = await Test.createTestingModule({
-        imports: [AppModule],
-    }).compile();
+  const module = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
 
-    app = module.createNestApplication();
-    await app.init();
-    return app;
+  app = module.createNestApplication();
+  await app.init();
+  return app;
 }
 
 export function getApp() {
-    return app;
+  return app;
 }
--- src/test-teardown.ts
+++ src/test-teardown.ts
@@ -2,8 +2,8 @@ import { NestApplication } from '@nestjs/core';
 import { getApp } from './test-setup';
 
 export default async function teardown() {
-    const app: NestApplication = getApp();
-    if (app) {
-        await app.close();
-    }
+  const app: NestApplication = getApp();
+  if (app) {
+    await app.close();
+  }
 }
